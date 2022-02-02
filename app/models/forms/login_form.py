388c25c97d69507add304716721d2e6d928e0ccc
skaylan <lucas_7tv@hotmail.com>
2022-02-02 17:25:51 -0300

    add autocomplete to login form

--- app/models/forms/login_form.py
+++ app/models/forms/login_form.py
@@ -5,6 +5,6 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Usuario', validators=[DataRequired()], render_kw={'placeholder': 'Username', 'autocomplete': 'off'})
+    username = StringField('Usuario', validators=[DataRequired()], render_kw={'placeholder': 'Username'})
     password = PasswordField('Senha', validators=[DataRequired()], render_kw={'placeholder': 'Password'})
     submit = SubmitField('Entrar', render_kw={'class': 'button'})
