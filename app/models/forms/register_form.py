468368daf90a77ebc2dee83b556f4ff8c100a99a
skaylan <lucas_7tv@hotmail.com>
2022-02-02 17:39:16 -0300

    pt-br to en

--- app/models/forms/register_form.py
+++ app/models/forms/register_form.py
@@ -5,9 +5,9 @@ from wtforms.validators import DataRequired, Email, Length
 
 
 class RegisterForm(FlaskForm):
-    name = StringField('nome', render_kw={'placeholder': 'Name'}, validators=[DataRequired(), Length(min=4, max=100, message='Nome deve conter entre 4 e 100 caracteres!')])
-    username = StringField('Usuario', render_kw={'placeholder': 'Username'}, validators=[DataRequired(), Length(min=4, max=30, message='Nome de usuario deve conter entre 4 e 30 caracteres')])
-    email = EmailField('Email', render_kw={'placeholder': 'Email'}, validators=[DataRequired(), Email(message='Email Invalido!')])
-    password = PasswordField('Senha', render_kw={'placeholder': 'Password'}, validators=[DataRequired(), Length(min=8, message="A senha deve no minimo 8 caracteres!")])
-    confirm_password = PasswordField('Confirmar senha', render_kw={'placeholder': 'Confirm password'}, validators=[DataRequired()])
+    name = StringField('Name', render_kw={'placeholder': 'Name'}, validators=[DataRequired(), Length(min=4, max=100, message='Name must contain between 4 and 100 characters')])
+    username = StringField('Username', render_kw={'placeholder': 'Username'}, validators=[DataRequired(), Length(min=4, max=30, message='Username must contain between 4 and 30 characters!')])
+    email = EmailField('Email', render_kw={'placeholder': 'Email'}, validators=[DataRequired(), Email(message='Invalid Email!')])
+    password = PasswordField('Password', render_kw={'placeholder': 'Password'}, validators=[DataRequired(), Length(min=8, message="The password must be at least 8 characters!")])
+    confirm_password = PasswordField('Confirm password', render_kw={'placeholder': 'Confirm password'}, validators=[DataRequired()])
     submit = SubmitField('Register', render_kw={'class': 'button'})
