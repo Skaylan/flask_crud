cd1f601e8aacec1b8fc3bd894706b3d9a316bd97
skaylan <lucas_7tv@hotmail.com>
2022-02-05 19:28:35 -0300

    some changes to login form

--- app/models/forms/login_form.py
+++ app/models/forms/login_form.py
@@ -5,6 +5,6 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Usuario', validators=[DataRequired()], render_kw={'placeholder': 'Username'})
-    password = PasswordField('Senha', validators=[DataRequired()], render_kw={'placeholder': 'Password'})
-    submit = SubmitField('Entrar', render_kw={'class': 'button'})
+    username = StringField('Username', validators=[DataRequired()], render_kw={'placeholder': 'Username'})
+    password = PasswordField('Password', validators=[DataRequired()], render_kw={'placeholder': 'Password'})
+    submit = SubmitField('Login', render_kw={'class': 'button'})
